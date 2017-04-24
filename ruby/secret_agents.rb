e59811e6bb52d7afffd725a9a1d2b3380d385962
Michael Glaser <m.glaser82@gmail.com>
2017-04-23 21:20:00 -0700

    Added user interface to program. Provides a promt to the user. Asked if they want to encrypt or decrypt. Can take input as full word or single letter. Can take input in either upper or lower case. Will continue to loop if one or the other is not chosen.

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,6 +3,7 @@
 
 #Encryption Method
 def encrypt(message)
+
   index = 0
   output = ""
   alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -18,12 +19,13 @@ def encrypt(message)
     output += alphabet[new_letter_number]
     index += 1
   end
-  p "Your crypto word is #{output}"
+  puts "Your encrypted word is #{output}"
 end
 
 #Decryption Method
 def decrypt(message)
   #puts "Insert message for crypto"
+
   index = 0
   output = ""
   alphabet = "abcdefghijklmnopqrstuvwxyz"
@@ -40,15 +42,43 @@ def decrypt(message)
     output += alphabet[new_letter_number]
     index += 1
   end
-  p "Your crypto word is #{output}"
+  puts "Your decrypted word is #{output}"
 end
 
-encrypt("abc")
-encrypt("zed")
-decrypt("bcd")
-decrypt("afe")
+#encrypt("abc")
+#encrypt("zed")
+#decrypt("bcd")
+#decrypt("afe")
 
-decrypt(encrypt("swordfish")) #This nested method call does not work
+#decrypt(encrypt("swordfish")) #This nested method call does not work
 #because the scope of the encrypted method does not extend to the decrypt
 #method. In other words, the two methods are separate and independent from
 #each other.
+
+puts "Good afternoon, Jim. Your mission, should you choose to accept it,
+is to decrypt or encrypt a password. As always, should you or any of your
+I.M. force be caught or killed, the Secretary will disavow any knowledge
+of your actions. This program will self destruct in 5 seconds.
+(I lied. No it won't.)"
+puts "Now, would you like to encrypt or decrypt a password? (E/D)"
+user_action = gets.chomp
+user_action = user_action[0]
+user_action = user_action.upcase
+encryption = false
+decryption = false
+until (encryption || decryption)
+  if user_action == "E"
+    encryption = true
+    puts "Please provide a password to be encrypted."
+    password = gets.chomp
+    encrypt(password)
+  elsif user_action == "D"
+    decryption = true
+    puts "Please provide an encrypted password to decrypt."
+    password = gets.chomp
+    decrypt(password)
+  else
+    encryption = false
+    decryption = false
+  end
+end

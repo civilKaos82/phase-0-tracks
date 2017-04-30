c18584ee42f0da6639128392064c16f8d7b58178
Michael Glaser <m.glaser82@gmail.com>
2017-04-30 16:34:49 -0700

    Updated file to include release 1. Added method to calulate fibonacci numbers given user input. Included check to verify if method is accurate to 100 fibonacci numbers.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,4 +1,6 @@
 #Michael H. Glaser paired w/ Jenna Ritten & Revecca Trachtenburg
+
+#Release 0
 def lengthsearch(search_array, search_integer)
   index = 0
   output = nil
@@ -13,13 +15,12 @@ def lengthsearch(search_array, search_integer)
   p output
 end
 
-#lengthsearch test
-puts "lengthsearch test"
-x = 1
-while x < 6
-lengthsearch([1, 2, 3, 4,], x)
-  x += 1
-end
+#puts "lengthsearch test"
+#x = 1
+#while x < 6
+#lengthsearch([1, 2, 3, 4,], x)
+#  x += 1
+#end
 
 def eachsearch(search_array, search_integer)
   index = 0
@@ -34,10 +35,39 @@ def eachsearch(search_array, search_integer)
   p output
 end
 
-#eachsearch test
-puts "eachsearch test"
-x = 1
-while x < 6
-eachsearch([1, 2, 3, 4,], x)
-  x += 1
+#puts "eachsearch test"
+#x = 1
+#while x < 6
+#eachsearch([1, 2, 3, 4,], x)
+#  x += 1
+#end
+
+#Release 1: Calculate Fibonacci Numbers
+
+def fib_term_generator(n)
+
+  fib_array = [0, 1]
+  index = 2
+  while n < index
+    p "A Fibonacci Sequence requires at least 2 numbers."
+    break if n < index
+  end
+
+  while index < n
+    fib_integer = fib_array[-1] + fib_array[-2]
+    fib_array << fib_integer
+    index += 1
+  end
+
+  fib_100 = 218922995834555169026
+
+  if fib_array[-1] == fib_100
+    p "The fibonacci sequence method is accurate to 100."
+  elsif index == n
+    p fib_array
+  end
+
 end
+
+puts "Input quantity of Fibonacci Numbers Desired."
+fib_term_generator(gets.chomp.to_i)

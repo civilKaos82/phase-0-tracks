b6a32efb77d9f74b31c417f1e5e61f47f1b95348
Michael Glaser <m.glaser82@gmail.com>
2017-05-07 13:13:39 -0700

    Added methods to the class puppy per release 0.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,28 @@ class Puppy
     toy
   end
 
+  def speak(barks)
+    barks.times {puts "WOOF!"}
+  end
+
+  def roll_over()
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_age = human_years * 7
+    puts "Ole is #{dog_age} in dog years."
+  end
+
+  def dog_action(options)
+    puts "Ole has decided to sleep instead of #{options}."
+  end
+
 end
 
+#Release 0:
+ole = Puppy.new
+ole.fetch("ball")
+ole.speak(gets.chomp.to_i)
+ole.dog_years(gets.chomp.to_i)
+ole.dog_action("sit")

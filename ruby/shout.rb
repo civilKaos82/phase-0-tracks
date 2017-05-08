b0745aaf6d8a5e2634c146cc7122e8e0bea2a64d
Michael Glaser <m.glaser82@gmail.com>
2017-05-07 21:04:08 -0700

    Finished release 3. Added two classes for an angry guy and a happy guy. Included shout module in both. Had both class iterations use both methods.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,29 @@
 module Shout
   # we'll put some methods here soon, but this code is fine for now!
-  def self.yell_angrily(words)
+  #I removed the self. before the yell_angrily below.
+  def yell_angrily(words)
     words + "!!!" + ":("
   end
-
-  def self.yell_happily(words)
+  #Here I also removed the self. before the method name.
+  def yell_happily(words)
     words + "!!!" + ":D"
   end
 end
 
-p Shout.yell_angrily("ARG")
-p Shout.yell_happily("YAY")
+#old code tests to see if shout would print the appropriate string for Release 1
+#p Shout.yell_angrily("ARG")
+#p Shout.yell_happily("YAY")
+class Angry_guy
+  include Shout
+end
+
+class Happy_guy
+  include Shout
+end
+
+angry_guy = Angry_guy.new
+p angry_guy.yell_angrily("ARG")
+p angry_guy.yell_happily("Yay")
+happy_guy = Happy_guy.new
+p happy_guy.yell_angrily("ARG")
+p happy_guy.yell_happily("Yay")

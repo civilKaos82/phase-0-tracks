6cb8da2a85d151a165887867e3f8c2f6c2f90a4d
mike <m.glaser82@gmail.com>
2017-05-14 23:16:48 -0700

    Added similarity check for objects. Added driver code to test output. Returns true or false based upon similarity of eye color, not name.

--- js/algos.js
+++ js/algos.js
@@ -9,8 +9,35 @@ function longestPhrase(array){
 			phrase = array[i];}
 		}
 		return phrase
-	}
+}
 
   var array = ["long phrase","longest phrase","longer phrase"];
 
   console.log(longestPhrase(array));
+
+//Release 1//
+function SimilarCheck (hash1, hash2) {
+   var similar = false;
+
+   for (var key1 in hash1) {
+	     for (var key2 in hash2) {
+		        if ((key1 === key2) && (hash1[key1] === hash2[key2])) {
+			            similar = true;}
+		}
+	}
+	return similar;
+}
+
+var TestObject1 = {name: "Mike", eyes: "brown"};
+var TestObject2 = {name: "Bob", eyes: "brown"};
+var TestObject3 = {name: "Bill", eyes: "blue"};
+var TestObject4 = {name: "Bill", eyes: "aqua"};
+
+console.log("Are", TestObject1, "and", TestObject2, "similar?");
+console.log(SimilarCheck(TestObject1, TestObject2));
+
+console.log("Are", TestObject2, "and", TestObject3, "similar?");
+console.log(SimilarCheck(TestObject2, TestObject3));
+
+console.log("Are", TestObject3, "and", TestObject4, "similar?");
+console.log(SimilarCheck(TestObject2, TestObject3));

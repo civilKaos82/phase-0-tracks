645fc4dfd51247c810a795bca4c61c02212d2227
mike <m.glaser82@gmail.com>
2017-05-14 22:36:24 -0700

    Added fuction for Cars, including make, model, color, & their ability to accelerate.

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,2 +1,29 @@
 var colors = ["Black", "Brown", "Grey", "White"];
-var names = ["Bob", "Bridget", "Jones", "Indiana"]
+var names = ["Bob", "Bridget", "Jones", "Indiana"];
+
+//add color to array
+colors.push("magenta");
+names.push("Yolo");
+
+var horses = {}
+
+
+for (var i = 0; i < colors.length; i++) {
+	horses[names[i]] = colors[i];
+}
+
+console.log(horses)
+
+//Cars
+
+function Cars(make, model, color){
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.accelerate = function () {console.log('The car begins to accelerate');}
+}
+
+var NewCar = new Cars("Chevy", "Blazer", "rusted");
+NewCar.accelerate()
+console.log(NewCar)
+console.log(new Cars("Ford", "Bronco", "more rusted"))

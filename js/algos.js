21eb74c51dd08f8dd791eeb61616d2ac8f645c65
mike <m.glaser82@gmail.com>
2017-05-14 22:58:32 -0700

    Added longest phrase function and driver code to test it.

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,16 @@
 //DBC 7.3 Algorithm Practice - Michael H. Glaser
 
 //Release 0//
+function longestPhrase(array){
+
+	var phrase = array[0];
+	for ( i=0 ; i < array.length ; i++) {
+		if ((array[i].length) > (phrase.length)) {
+			phrase = array[i];}
+		}
+		return phrase
+	}
+
+  var array = ["long phrase","longest phrase","longer phrase"];
+
+  console.log(longestPhrase(array));

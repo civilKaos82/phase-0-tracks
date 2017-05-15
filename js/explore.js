2bd96a62ca67582b89238935f86533351aa343e4
mike <m.glaser82@gmail.com>
2017-05-14 22:08:47 -0700

    Initial commit of explore.js with psudocode.

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Michael H. Glaser 7.1
+// Pseudocode:
+// Create an empty string "ReversedString" that will take a newly created reversed string
+// loop from the length of the string minus 1 to 0, using the i-- (which decreases the increment by 1)
+// Take the reversed string ReversedString and add the letters into the new variable without deleting them
+// by using +=
